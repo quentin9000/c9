7fb15c402d3dccce7f03c9771100bcb928e133fb
quentin9000 <134021398+quentin9000@users.noreply.github.com>
2023-05-25 19:22:37 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
-var sprite1
+var box;
 function setup() {
   createCanvas(400,400);
- sprite1= createSprite (50,50,50,50)
+ box = createSprite (200,200,20,20)
   
 }
 
 function draw() 
 {
-  background("red");
-drawSprites()
+  background(30);
+
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    box.position.x = box.position.x +5;
+  }
+  if (keyIsDown(LEFT_ARROW))
+  {
+    box.position.x = box.position.x -5
+  }
+
+  if (keyIsDown(UP_ARROW))
+  {
+    box.position.y = box.position.y -5;
+  }
+
+  if (keyIsDown(DOWN_ARROW))
+  {
+    box.position.y = box.position.y + 5;
+  }
+drawSprites();
 }
 
 
